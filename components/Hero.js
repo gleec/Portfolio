be419735adc31931babb406bac44e0b506bc0e27
Glery Cardoza <gleryc8@gamil.com>
2022-03-16 14:36:07 -0400

    scss  corrections in hero

--- components/Hero.js
+++ components/Hero.js
@@ -10,16 +10,16 @@ const Hero = () => {
       <div className="row">
         <div className="col-md-4 px-0">
           <div className="px-4 card h-100">
-            <div className="card-body">
-              <h1 className="fw-light pb-3">{t('skills')}</h1>
+            <div className="py-4">
+              <h1 className="fw-light pb-4">{t('skills')}</h1>
                <Skills/>
             </div>
           </div>
         </div>
         <div className="col-md-8 px-0">
           <div className="h-100 card px-4">
-            <div className="card-body d-inline-block">
-              <h1 className="fw-light pb-3">{t('experience')}</h1>
+            <div className="d-inline-block py-4">
+              <h1 className="fw-light pb-4">{t('experience')}</h1>
               <div>
                <Experiences/>
               </div>
